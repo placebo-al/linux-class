d670063b29dec1fba32202d71c4eec92cce09e5c
placebo-al <placebo_al@hotmail.com>
2024-12-31 16:37:09 +1100

    I have added some extra install.sh for the folders that were missing the files, and added the call to the vagrantfiles

---
+++ icinga/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+then 
+	echo "Please run with sudo or as root"
+	exit 1
+fi
+
+
+# dnf install -y httpd php php-gd php-intl php-ldap php-opcache
+# cp /etc/php.ini /etc/php.ini.bak
+# vi /etc/php.ini
+# systemctl enable --now httpd
+# dnf install -y mariadb-server
+# systemctl enable --now mariadb
+# mysql_secure_installation
+# mysqladmin -u root -p create icinga
+# mysqladmin -u root -p create icingaweb
+# mysql -u root -p
+# dnf install -y http://mirror.linuxtrainingacademy.com/icinga/icinga-rpm-release.noarch.rpm
+# dnf install -y icinga2 icingaweb2 icingacli icinga2-ido-mysql
+# mysql -u root -p icinga < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+# mysqlshow -u root -p icinga
+# vi /etc/icinga2/features-available/ido-mysql.conf
+# icinga2 feature enable ido-mysql
+# icinga2 feature list
+# dnf install -y epel-release
+# dnf config-manager --set-enable powertools
+# dnf install -y nagios-plugins-all
+# icinga2 node wizard
+# systemctl start icinga2
+# systemctl enable icinga2
+# systemctl restart httpd
+# cat /etc/icinga2/conf.d/api-users.conf
+# icingacli setup token create
