72d52c61d401e20a5c6638fd08acc634a48367eb
placebo-al <placebo_al@hotmail.com>
2024-12-31 14:55:02 +1100

    I have started to set up the automated version of the commands, including changing the UID check and the automated echo input

--- kanboard/install.sh
+++ kanboard/install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+then 
+	echo "Please run with sudo or as root"
+	exit 1
+fi
+
 sudo dnf install -y httpd php php-mysqlnd php-gd php-mbstring php-json php-xml
 
 sudo systemctl start httpd
@@ -8,20 +16,21 @@ sudo systemctl start mariadb-server
 sudo systemctl enable mariadb-server
 
 sudo mysql_secure_installation
-# have to add the input command
+echo "Running Mysql_secure_installation..."
+echo -e "\n\nPassword-to-change\nPassword-to-change\n\n\n\n\n" | mysql_secure_installation
 
-mysqladmin -u root -p create kanboard
+mysqladmin create kanboard
 
-# mysql -u root -p
-# GRANT ALL on kanboard.* to kanboard@localhost identified by '<password>';
-# FLUSH PRIVILEGES;
-# exit
+mysql -e "GRANT ALL on kanboard.* to kanboard@localhost identified by '<password>';"
+mysql -e "FLUSH PRIVILEGES;"
+
 
 curl -LO http://mirror.linuxtrainingacademy.com/kanboard/kanboard-v1.2.15.zip
 
 unzip kanboard-v1.2.15.zip
 
 mv kanboard-v1.2.15/* /var/www/html/
+
 
 echo "
 ?php
