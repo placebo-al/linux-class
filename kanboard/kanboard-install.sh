0ac0d6fd97e7a46e46598de442cea75a3539c8fa
placebo-al <placebo_al@hotmail.com>
2025-01-08 13:07:44 +1100

    I have added some extra details due to the vagrant set up failing, and this now appears to have fixed the issue, adding the more explicit user for the SQL changes, and a few small adjustments seems to have done the trick

--- kanboard/kanboard-install.sh
+++ kanboard/kanboard-install.sh
@@ -6,40 +6,43 @@ then
 	exit 1
 fi
 
+
 dnf install -y httpd php php-mysqlnd php-gd php-mbstring php-json php-xml
 
 systemctl start httpd
 systemctl enable httpd
 
 dnf install -y mariadb-server
-systemctl start mariadb-server
-systemctl enable mariadb-server
+systemctl start mariadb
+systemctl enable mariadb
+
 
 ### mysql_secure_installation using the echo input
 echo "Running Mysql_secure_installation..."
-echo -e "\n\nPassword-to-change\nPassword-to-change\n\n\n\n\n" | mysql_secure_installation
+echo -e "\n\nPassword123\nPassword123\n\n\n\n\n" | mysql_secure_installation
 
-mysqladmin create kanboard
 
-mysql -e "GRANT ALL on kanboard.* to kanboard@localhost identified by '<password>';"
-mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -pPassword123 create kanboard
+
+mysql -u root -pPassword123 -e "GRANT ALL on kanboard.* to kanboard@localhost identified by 'Password123';"
+mysql -u root -pPassword123 -e "FLUSH PRIVILEGES;"
 
 
 curl -LO http://mirror.linuxtrainingacademy.com/kanboard/kanboard-v1.2.15.zip
 
 unzip kanboard-v1.2.15.zip
 
-mv kanboard-v1.2.15/* /var/www/html/
+mv kanboard-1.2.15/* /var/www/html/
 
 
-echo "
-?php
+echo "<?php
 define('DB_DRIVER', 'mysql');
 define('DB_USERNAME', 'kanboard');
-define('DB_PASSWORD', '<password>');
+define('DB_PASSWORD', 'Password123');
 define('DB_HOSTNAME', 'localhost');
 define('DB_NAME', 'kanboard');
 " >> /var/www/html/config.php
+
 
 
 
