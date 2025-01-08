16269f089e1784dc308a9775d77ca5a950839009
placebo-al <placebo_al@hotmail.com>
2025-01-08 17:04:35 +1100

    I have added the basic LAMP configuration to the hesk install file

--- hesk/hesk-install.sh
+++ hesk/hesk-install.sh
@@ -6,3 +6,23 @@ then
   exit 1
 fi
 
+# Install apache and php
+dnf install -y httpd php php-mysqlnd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Install Mariadb
+dnf install -y mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+### mysql_secure_installation using the echo input
+echo "Running Mysql_secure_installation..."
+echo -e "\n\nPassword123\nPassword123\n\n\n\n\n" | mysql_secure_installation
+
+# Create the Hesk database
+mysqladmin -u root -pPassword123 create hesk
+
+mysql -u root -pPassword123 -e "GRANT ALL on hesk.* to hesk@localhost identified by 'Password123'"
+mysql -u root -pPassword123 -e "FLUSH PRIVILEGES;"
+
