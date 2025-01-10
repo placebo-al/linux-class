738b87986e91332d55407fa2ba7e66cbfaa748ac
placebo-al <placebo_al@hotmail.com>
2025-01-10 16:04:51 +1100

    I have tidied two of the vagrantfiles, and added the configuration for the phpMyAdmin install file

--- phpmyadmin/phpmyadmin-install.sh
+++ phpmyadmin/phpmyadmin-install.sh
@@ -6,3 +6,35 @@ then
 	exit 1
 fi
 
+dnf install -y httpd
+dnf install -y php php-mysqlnd php-json php-pecl-zip php-mbstring 
+
+systemctl start httpd
+systemctl enable httpd
+
+dnf install -y mariadb-server
+
+### mysql_secure_installation using the echo input
+echo "Running Mysql_secure_installation..."
+echo -e "\n\nPassword123\nPassword123\n\n\n\n\n" | mysql_secure_installation
+
+curl -LO http://mirror.linuxtrainingacademy.com/phpMyAdmin/phpMyAdmin-5.0.2-all-languages.zip
+
+unzip phpMyAdmin-5.0.2-all-languages.zip
+
+mv phpMyAdmin-5.0.2-all-languages/* /var/www/html
+
+cp /var/www/html/config.sample.inc.php /var/www/html/config.inc.php
+
+# Need to save the value from 
+value = $(date | md5sum)
+
+# Add that to the file
+vi /var/www/html/config.inc.php
+
+# Under the value
+$cfg['blowfish_secret'] = "$value";
+
+mkdir /var/www/html/tmp
+chown apache /var/www/html/tmp
+
